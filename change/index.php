5d0c4f9a0e3e81032afc27d8ac3490ec89003dc5
DearPear <somehow@live.fr>
2020-02-04 21:10:10 +0100

    fix tests change

--- change/index.php
+++ change/index.php
@@ -18,9 +18,22 @@ if(testchange($change, false, $testpassed)){
 }
 echo "<br>";
 
-$val = 119;
+$val = 15;
 $change1 = change($val);
-if(testchange($change1, array("billet10" => 0, "billet5" => 0, "piece2" => 0), $testpassed)){
+if(testchange($change1, array("billet10" => 1, "billet5" => 1, "piece2" => 0), $testpassed)){
+    echo "<br>";
+    echo "succès";
+    echo "<br>";
+} else{
+    echo "<br>";
+    echo "echec";
+    echo "<br>";
+}
+echo "<br>";
+
+$val = 87;
+$change1 = change($val);
+if(testchange($change1, array("billet10" => 8, "billet5" =>1, "piece2" => 1), $testpassed)){
     echo "<br>";
     echo "succès";
     echo "<br>";
@@ -32,7 +45,31 @@ if(testchange($change1, array("billet10" => 0, "billet5" => 0, "piece2" => 0), $
 echo "<br>";
 
 
+$val = 89;
+$change1 = change($val);
+if(testchange($change1, array("billet10" => 8, "billet5" => 1, "piece2" => 2), $testpassed)){
+    echo "<br>";
+    echo "succès";
+    echo "<br>";
+} else{
+    echo "<br>";
+    echo "echec";
+    echo "<br>";
+}
+echo "<br>";
 
+$val = 4;
+$change1 = change($val);
+if(testchange($change1, array("billet10" => 0, "billet5" => 0, "piece2" => 2), $testpassed)){
+    echo "<br>";
+    echo "succès";
+    echo "<br>";
+} else{
+    echo "<br>";
+    echo "echec";
+    echo "<br>";
+}
+echo "<br>";
 
 if($testpassed){
     echo "<br>";
